6f09397f64d629e9bc553aa65a94449f8b1e5398
amarshssingh0011 <amarshssingh@gmail.com>
2026-07-07 13:36:31 +0530

    Refactor app.py for model path and formatting
    Updated model loading path and ensured code formatting consistency.

--- PROJECT 4 CAT DOG MODEL/PROJECT-4-main/app.py
+++ PROJECT 4 CAT DOG MODEL/PROJECT-4-main/app.py
@@ -15,7 +15,7 @@ st.set_page_config(
 # -------------------------
 # Load Model
 # -------------------------
-model = joblib.load("cat_dog_model.pkl")
+model = joblib.load("PROJECT 4 CAT DOG MODEL/PROJECT-4-main/cat_dog_model.pkl")
 
 IMG_SIZE = 64
 
